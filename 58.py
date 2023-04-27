d29dc199e28fe35c732e1fea4cb0c1f1cb58da0f
alladheeraj <56075562+alladheeraj@users.noreply.github.com>
2023-04-27 18:48:32 +0530

    Create 58.py

---
+++ 58.py
@@ -0,0 +1,7 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.rstrip()
+        i = len(s) - 1
+        while i >= 0 and s[i] != ' ':
+            i -= 1
+        return len(s) - 1 - i
